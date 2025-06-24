bd0142a08d5bd700fc96d8189317011a3292e55a
aarsri <43250555+aarsri@users.noreply.github.com>
2025-06-24 19:06:03 -0400

    Update footer.tsx

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer>
-      © 2015–2021 David Chiang. Unless otherwise indicated, all materials are licensed under a Creative Commons Attribution 4.0 International License.
+      © 2025 Aarohi Srivastava. Unless otherwise indicated, all materials are licensed under a Creative Commons Attribution 4.0 International License.
     </footer>
   );
 }
