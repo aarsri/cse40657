87c1538d670c14238d7de7fa511fbfcf8d96865e
aarsri <43250555+aarsri@users.noreply.github.com>
2025-06-24 18:48:39 -0400

    Update overview.tsx

--- src/components/templates/overview.tsx
+++ src/components/templates/overview.tsx
@@ -14,12 +14,12 @@ const Overview = () => {
             <b>Term:</b> Fall 2025
           </p>
           <p>
-            <b>Time:</b> Mon, Wed, & Fri @ 11:30-12:20
+            <b>Time:</b> MWF 11:30-12:20
           </p>
         </div>
         <div className="row-area-small">
           <p>
-            <b>Location:</b> DeBartolo XXX
+            <b>Location:</b> DeBartolo 310
           </p>
           <p>
             <b>Instructor:</b> Aarohi Srivastava
@@ -36,10 +36,10 @@ const Overview = () => {
             <b>Term:</b> Fall 2025
           </p>
           <p>
-            <b>Time:</b> Mon, Wed, & Fri @ 11:30-12:20
+            <b>Time:</b> MWF 11:30-12:20
           </p>
           <p>
-            <b>Location:</b> DeBartolo XXX
+            <b>Location:</b> DeBartolo 310
           </p>
           <p>
             <b>Instructor:</b> Aarohi Srivastava
@@ -64,14 +64,14 @@ const Overview = () => {
               <b>Office Location:</b> TBA
             </p>
             <p>
-              <b>Pronunciation:</b> Aarohi Srivastava
+              <b>Pronunciation:</b> ah-ROH-hee shree-VAHS-tuhv
             </p>
           </div>
         </article>
         
         <article>
           <h2 className="heading">
-            TA - Contact Info
+            TA: TBA
           </h2>
 
           <div className="row-area-small">
@@ -87,7 +87,7 @@ const Overview = () => {
               <b>Office Location:</b> TBA
             </p>
             <p>
-              <b>Pronunciation:</b> Aarohi Srivastava
+              <b>Pronunciation:</b> ah-ROH-hee shree-VAHS-tuhv
             </p>
           </div>
         </article>
@@ -95,7 +95,7 @@ const Overview = () => {
 
       <article>
         <h2 className="heading">
-          What is Natural Language Processing?
+          Course Description
         </h2>
         <p>
           Computers process massive amounts of information every day in the form of human
@@ -103,14 +103,6 @@ const Overview = () => {
           answer questions about it, or translate it into other languages. This course is a
           systematic introduction to the ideas that form the foundation of current language
           technologies and research into future language technologies.
-        </p>
-
-        <p>
-          The official prerequisite is CSE 20312. Students should be experienced with writing
-          substantial programs in Python. The course also makes use of finite automata, context-free
-          grammars, basic linear algebra, multivariable differential calculus, and probability theory.
-          Ideally, students should have taken CSE 30151, Math 10560, and ACMS 30440, but please contact
-          the instructor if you have questions about the necessary background.
         </p>
       </article>
     </section>
